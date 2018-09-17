0292ef6ca90257053ee324d3e89f35f06c450b55
flyingantman <flyingantman@gmail.com>
2018-09-16 23:04:50 -0400

    Menu has been implemented

---
+++ Miss Ways/Assets/MissWays/Scripts/ShowMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMenu : MonoBehaviour {
+
+    [SerializeField]
+    List<GameObject> UIToHide;
+    [SerializeField]
+    List<bool> UIStatus;
+    [SerializeField]
+    List<bool> UIMenuScreen;
+
+    public void MenuScreen()
+    {
+        for (int i = 0; i < UIStatus.Count; i++)
+        {
+            UIStatus[i] = UIToHide[i].activeSelf;
+        }
+
+        for (int i = 0; i < UIToHide.Count; i++)
+        {
+            UIToHide[i].SetActive(UIMenuScreen[i]);
+        }
+    }
+
+    public void CameraView()
+    {
+        for (int i = 0; i < UIToHide.Count; i++)
+        {
+            UIToHide[i].SetActive(UIStatus[i]);
+        }
+    }
+}
