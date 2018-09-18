a8636c3464258d67c48d7290985f0d135587beee
flyingantman <flyingantman@gmail.com>
2018-09-18 00:58:11 -0400

    Added Local Info screen, updated coachmarks, resized video player prefab to fill whole card in landscape

---
+++ Miss Ways/Assets/MissWays/Scripts/ShowInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowInfo : MonoBehaviour
+{
+    Toggle t;
+
+    public GameObject info;
+
+    void Awake()
+    {
+        t = gameObject.GetComponent<Toggle>();
+    }
+
+    public void ToggleInfo()
+    {
+        info.SetActive(t.isOn);
+    }
+}
